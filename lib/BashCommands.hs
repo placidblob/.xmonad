41f1405fbb5400c962d8ef7ff6ab98bb935da533
Yannis.Kalligeros <yak@intuitionrobotics.com>
2019-03-30 22:50:33 +0200

    .

--- lib/BashCommands.hs
+++ lib/BashCommands.hs
@@ -1,30 +1,34 @@
 module BashCommands (
   cmdLauncher
+
 , cmdTerminal
 , cmdTerminator
+
 , cmdScreenshot
+
+, cmdLock
+
 , cmdPlayPause
 , cmdNext
 , cmdPrev
-, cmdLock
 , cmdVolUp
 , cmdVolDown
 , cmdToggleMute
-, cmdSpotifyPlayPause
 ) where
 
   cmdLauncher   = "rofi -show run"
 
   cmdTerminal   = "konsole"
+
   cmdTerminator = "terminator"
   cmdScreenshot = "spectacle"
+
+  cmdLock       = "i3lock -f -r5 -s3"
 
   cmdPlayPause  = "playerctl play-pause"
   cmdNext       = "playerctl next"
   cmdPrev       = "playerctl previous"
-  cmdLock       = "i3lock -f -r5 -s3"
   cmdVolUp      = "amixer -q set Master 5%+"
   cmdVolDown    = "amixer -q set Master 5%-"
   cmdToggleMute = "amixer -q set Master toggle"
 
-  cmdSpotifyPlayPause = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
